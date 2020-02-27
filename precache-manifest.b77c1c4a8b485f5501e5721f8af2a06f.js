d3316cddd76836d963a63eab3e157a6920954d59
Jasan Hernandez <jasan_azael@hotmail.es>
2020-02-27 17:38:32 -0600

    Updates

---
+++ precache-manifest.b77c1c4a8b485f5501e5721f8af2a06f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d5e6c51ba8a9b797281",
+    "url": "/rickAndMorty/static/css/main.ce103bae.chunk.css"
+  },
+  {
+    "revision": "6d5e6c51ba8a9b797281",
+    "url": "/rickAndMorty/static/js/main.4091811b.chunk.js"
+  },
+  {
+    "revision": "743771835d854e8a3922",
+    "url": "/rickAndMorty/static/js/runtime~main.94f5a8de.js"
+  },
+  {
+    "revision": "df70d2b55a030608f2f2",
+    "url": "/rickAndMorty/static/js/2.656ad806.chunk.js"
+  },
+  {
+    "revision": "de82c64b0013c1e2a22f5a8fb26f04f4",
+    "url": "/rickAndMorty/static/media/logo.de82c64b.png"
+  },
+  {
+    "revision": "a21b9669e4feae505611d168cbd853d8",
+    "url": "/rickAndMorty/index.html"
+  }
+];
